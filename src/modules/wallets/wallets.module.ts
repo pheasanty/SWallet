5685dd36aea0e73563de910ec619652fafcaf764
pheasanty <jose_ricardosr98@hotmail.com>
2025-09-21 00:22:46 -0400

    feat: Add WalletBalancesService to WalletsModule for balance management

--- src/modules/wallets/wallets.module.ts
+++ src/modules/wallets/wallets.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { WalletsService } from '../../services/wallets.service';
+import { WalletBalancesService } from '../../services/wallet-balances.service';
 import { WalletsController } from './wallets.controller';
 import { Wallet } from '../../entities/wallet.entity';
 import { WalletBalance } from '../../entities/wallet-balance.entity';
@@ -8,7 +9,7 @@ import { WalletBalance } from '../../entities/wallet-balance.entity';
 @Module({
   imports: [TypeOrmModule.forFeature([Wallet, WalletBalance])],
   controllers: [WalletsController],
-  providers: [WalletsService],
-  exports: [WalletsService],
+  providers: [WalletsService, WalletBalancesService],
+  exports: [WalletsService, WalletBalancesService],
 })
 export class WalletsModule {}
